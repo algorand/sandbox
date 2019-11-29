ec0c723747eb4f39fd014e27e614039d58d78d59
Will Winder <wwinder.unh@gmail.com>
2019-11-29 17:43:03 -0500

    Update transaction tutorial

--- utils/transaction.sh
+++ utils/transaction.sh
@@ -33,13 +33,17 @@ white=`echo -en "\e[97m"`
 default=`echo -en "\e[39m"`
 
 
+PROMPT="${blue}${GRAY}~$ ${DEFAULT}${default}"
 
 function executeUntil {
   while true; do
-    read response
-    if [[ $response == $1 ]]; then
+    read -p "$PROMPT${green}" response
+    if [[ $response == $1*  ]]; then
+      printf "${default}"
+      eval $response
       return
     else
+      printf "${default}"
       eval $response
     fi
   done
@@ -51,70 +55,93 @@ function execute {
 }
 
 cat << EOF
-In this tutorial you will use goal to create a wallet, create two accounts in
-the wallet, request Algos from the bank, and send Algos between your accounts.
+In this tutorial you will use the 'goal' command to create a wallet, create
+two accounts in the wallet, request Algos from the bank, and send Algos between
+your accounts.
 
 To run this tutorial you will need a testnet sandbox with a fully processed
 ledger. If you haven't done that, you can run the command:
+${red}'./sandbox clean'${default}
 ${red}'./sandbox up testnet --use-snapshot'${default}
 
-How to use this tutorial
-${red}----------------------${default}
+${red}----------------------------
+| How to use this tutorial |
+----------------------------${default}
 
 This is an interactive tutorial which follows a number of commands designed to
 complete the task. Each command is a step of the tutorial and will contain an
 explanation of what will happen, followed by the command in square brackets.
 
-When prompted with the next command, any line you enter in the script will be
-executed. This is so you can experiment with what is being explained. When
-finished, enter an empty line to execute the provided command.
-${red}----------------------${default}
+When you see the $PROMPT anything you enter will be executed, this way you can
+experiment with what is described. When you're ready to continue, enter the
+command in square brackets.
 
-Interacting with the Key Management Daemon.
+${red}--------
+| Goal |
+--------${default}
+
+The goal utility is used to interact with the Algorand software. It coordinates
+different daemons and has many tools to automate various tasks. The sandbox
+script provides a "goal" option to forward these commands to the sandbox.
+
+Try running this simple goal command to get information about your node
+
+${aqua}[./sandbox goal node status]${default}
+
+EOF
+
+executeUntil "./sandbox goal node status"
+
+cat << EOF
+
+${red}----------------------------------------------
+| Interacting with the Key Management Daemon |
+----------------------------------------------${default}
 
 Algorand accounts are secured inside a wallet and managed by a key management
 daemon, named KMD. You can interact with KMD using the 'goal' command. Lets
 check if there are any wallets registered with your node:
 
 ${aqua}[./sandbox goal wallet list]${default}
+
 EOF
 
-executeUntil ""
-execute "./sandbox goal wallet list"
+executeUntil "./sandbox goal wallet list"
 
 cat << EOF
-${red}----------------------${default}
 
-Creating a wallet.
+${red}---------------------
+| Creating a wallet |
+---------------------${default}
 
 That command probably wasn't very interesting, lets create a wallet.
 
-${aqua}[./sandbox goal wallet new test-wallet]${default}
+${aqua}[./sandbox goal wallet new <wallet-name>]${default}
 
 EOF
 
-executeUntil ""
-execute "./sandbox goal wallet new test-wallet"
+executeUntil "./sandbox goal wallet new"
 
 cat << EOF
-${red}----------------------${default}
 
-Listing your wallet(s).
+${red}--------------------------
+| Listing your wallet(s) |
+--------------------------${default}
 
-Now we can list the wallets and see something more interesting. Try creating a"
+Now we can list the wallets and see something more interesting. Try creating a
 couple and see how the results change.
 
 ${aqua}[./sandbox goal wallet list]${default}
 
 EOF
 
-executeUntil ""
-./sandbox goal wallet list
+executeUntil "./sandbox goal wallet list"
 
 cat << EOF
-${red}----------------------${default}
 
-Creating an account.
+${red}-----------------------
+| Creating an account |
+-----------------------${default}
 
 Now that we have a wallet (and hopefully set it as the default wallet), we can
 create some accounts.
@@ -123,13 +150,13 @@ ${aqua}[./sandbox goal account new ; ./sandbox goal account new]${default}
 
 EOF
 
-executeUntil ""
-execute "./sandbox goal account new ; ./sandbox goal account new"
+executeUntil "./sandbox goal account new"
 
 cat << EOF
-${red}----------------------${default}
 
-Listing the accounts.
+${red}------------------------
+| Listing the accounts |
+------------------------${default}
 
 Similar to how you would list your wallets, you can list your accounts.
 
@@ -137,13 +164,13 @@ ${aqua}[./sandbox goal account list]${default}
 
 EOF
 
-executeUntil ""
-execute "./sandbox goal account list"
+executeUntil "./sandbox goal account list"
 
 cat << EOF
-${red}----------------------${default}
 
-Using the Bank.
+${red}------------------
+| Using the Bank |
+------------------${default}
 
 A Bank service is available to dispense Algos on Algorand test networks. Try
 using one of them to fund one of your accounts:
@@ -158,27 +185,50 @@ Bank transaction has been confirmed.
 ${aqua}[./sandbox goal account list]${default}
 
 EOF
-executeUntil ""
-execute "./sandbox goal account list"
+
+executeUntil "./sandbox goal account list"
 
 # TODO: Capture the output of 'goal account list' to ensure that an account has
 #       been funded, and record the addresses for use in the next command.
 
 cat << EOF
-${red}----------------------${default}
 
-Making a transaction.
+${red}------------------------
+| Making a transaction |
+------------------------${default}
 
-You should now be the proud owner of 100000000 microAlgos, lets try to create
-a transaction with them. You'll need to help out with this one, I can't seem to
-recall which account you funded. Fill in the blanks on this command to create a
-transaction then list your accounts to see that the balances have been updated:
+You should now be the proud owner of 100000000 microAlgos, lets create a
+transaction with them. Fill in the blanks with your account addresses (the 58
+character string) to send a transaction:
 
-./goal clerk send -a <amount-of-microAlgos> -f <from-account> -t <to-account>
+${aqua}./sandbox goal clerk send -a <amount-of-microAlgos> -f <from-account> -t <to-account>${default}
+
+EOF
+
+executeUntil "./sandbox goal clerk send"
+
+
+cat << EOF
+
+${red}--------------------------
+| Verify the transaction |
+--------------------------${default}
+
+List the accounts one more time to verify that your transaction has been sent.
 
 ${aqua}[./sandbox goal account list]${default}
 
 EOF
 
-executeUntil ""
-execute "./sandbox goal account list"
+executeUntil "./sandbox goal account list"
+
+cat << EOF
+
+${red}---------------
+| Wrapping up |
+---------------${default}
+
+You can find more tutorials in the developer documentation:
+https://developer.algorand.org/docs/tutorials
+
+EOF
