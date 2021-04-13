afef8e666dcb6fd496a1e65f551c2ab1fd7e506c
Kevin Lai <kevlai22@uw.edu>
2021-04-13 16:35:53 +0800

    feat: added algosearch support

---
+++ images/search/install.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+# Build AlgoSearch and put binary in /tmp.
+#
+# Configured with environment variables:
+#   URL    - Git repository URL.
+#   BRANCH - Git branch to clone.
+#   SHA    - (optional) Specific commit hash to checkout.
+set -e
+
+# Sometimes AlgoSearch is disabled, detect the missing build config.
+if [ -z "${BRANCH}" ] || [ -z "${URL}" ]; then
+  echo "Missing BRANCH or URL environment variable. Skipping install."
+  exit 0
+fi
+
+#git clone --single-branch --branch "${BRANCH}" "${URL}" /opt/algosearch
+git clone --single-branch --branch "${BRANCH}" "${URL}" /app
+if [ "${SHA}" != "" ]; then
+  echo "Checking out ${SHA}"
+  git checkout "${SHA}";
+fi
+echo << "fuck"
+#ls -l /opt/algosearch
+#cp /opt/algosearch /app
