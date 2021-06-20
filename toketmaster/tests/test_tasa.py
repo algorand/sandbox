14463570c393bfea3cbe73376fe988d244d56312
stepanulyanin <stepan.ulyanin@gmail.com>
2021-06-20 16:42:39 -0700

    Add the test case for tasa creation

--- toketmaster/tests/test_tasa.py
+++ toketmaster/tests/test_tasa.py
@@ -1,12 +1,47 @@
+import time
 import unittest
 
-from toketmaster.src import tasa
-from toketmaster.src import models
+from algosdk.error import IndexerHTTPError
+
+from toketmaster.src import client, models, tasa
 
 
 class TestTasa(unittest.TestCase):
     def setUp(self) -> None:
-        toket
+        self.toketmaster_wallet = models.Wallet(
+            public_key='PGFSRHMDZ2FM7GGTIAXU7BGPJENEARFCU2SYE2DLD44EIU2Z6VPF7AQEPQ',
+            secret_key='KONd7E3fz4p6ar2FzYDivkcBhDeEQKq/qtCIty9+trR5iyidg86Kz5jTQC9PhM9JGkBEoqalgmhrHzhEU1n1Xg=='
+        )
 
     def test_tasa_is_created(self):
-        tasa.create_tasa()
+        tasa_creation_id = tasa.create_tasa(creator=self.toketmaster_wallet,
+                                            manager=self.toketmaster_wallet,
+                                            freeze=self.toketmaster_wallet,
+                                            reserve=self.toketmaster_wallet,
+                                            clawback=self.toketmaster_wallet,
+                                            fee=5,
+                                            unit_name="TASA",
+                                            asset_name="Domvanin",
+                                            url="domvanin.org")
+
+        # transaction doesn't post immediately
+        while True:
+            try:
+                r = client.indexer_client.transaction(txid=tasa_creation_id)
+                break
+
+            except IndexerHTTPError:
+                time.sleep(1)
+
+        tx_params = r['transaction']['asset-config-transaction']['params']
+
+        assert tx_params['creator'] == self.toketmaster_wallet.public_key
+        assert tx_params['manager'] == self.toketmaster_wallet.public_key
+        assert tx_params['clawback'] == self.toketmaster_wallet.public_key
+        assert tx_params['reserve'] == self.toketmaster_wallet.public_key
+        assert tx_params['freeze'] == self.toketmaster_wallet.public_key
+        assert tx_params['total'] == 1
+        assert tx_params['unit-name'] == 'TASA'
+        assert tx_params['name'] == 'Domvanin'
+        assert tx_params['url'] == 'domvanin.org'
+
